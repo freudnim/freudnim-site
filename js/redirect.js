88893f011e5671ae3793505fc499b19d67ce4e38
freudnim <freud.nim@gmail.com>
2024-11-01 00:20:01 -0400

    refactor

--- js/redirect.js
+++ js/redirect.js
@@ -13,48 +13,50 @@ const homepage =
     (location.port ? ":" + location.port : "") +
     "/" +
     location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
+
 if (keyword) {
     const xhr = new XMLHttpRequest();
     xhr.onload = redirect;
     xhr.onerror = onError;
     xhr.open("GET", GITHUB_ISSUES_LINK);
     xhr.send();
-}
 
-function redirect() {
-    try {
-        const payload = JSON.parse(xhr.response);
-        const message = payload.message;
-        let link = "";
-        for (let i = 0; i < payload.length; i++) {
-            const issue = payload[i];
-            const issueBody = encodeURIComponent(issue.body);
-            if (issueBody == keyword) {
-                link = issue.title;
-                break;
+    function redirect() {
+        try {
+            const payload = JSON.parse(xhr.response);
+            const message = payload.message;
+
+            let link = "";
+            for (let i = 0; i < payload.length; i++) {
+                const issue = payload[i];
+                const issueBody = encodeURIComponent(issue.body);
+                if (issueBody == keyword) {
+                    link = issue.title;
+                    break;
+                }
             }
-        }
-        // Workaround IE 11 lack of support for new URL()
-        const url = document.createElement("a");
-        url.setAttribute("href", link);
+            // Workaround IE 11 lack of support for new URL()
+            const url = document.createElement("a");
+            url.setAttribute("href", link);
 
-        // Invalid URLs includes invalid issue numbers, issue titles that are not URLs, and recursive destination URLs
-        const isInvalidUrl =
-            message === "Not Found" ||
-            !link ||
-            !isUrl(link) ||
-            url.hostname === location.hostname;
+            // Invalid URLs includes invalid issue numbers, issue titles that are not URLs, and recursive destination URLs
+            const isInvalidUrl =
+                message === "Not Found" ||
+                !link ||
+                !isUrl(link) ||
+                url.hostname === location.hostname;
 
-        if (isInvalidUrl) {
+            if (isInvalidUrl) {
+                location.replace(homepage);
+            } else {
+                location.replace(link);
+            }
+        } catch (e) {
             location.replace(homepage);
-        } else {
-            location.replace(link);
         }
-    } catch (e) {
+    }
+
+    function onError() {
         location.replace(homepage);
     }
-}
-
-function onError() {
-    location.replace(homepage);
 }
