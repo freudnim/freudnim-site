f99ea0b5a4b568a054ff236e38b8a3cb746998ea
freudnim <freud.nim@gmail.com>
2025-12-14 05:26:00 -0500

    fix asset prefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: "",
-  assetPrefix: "",
+  assetPrefix: ".", // use relative paths for _next/static
   images: {
     unoptimized: true, // REQUIRED for GitHub Pages
   },
