da05efaf7affee0bdbdf9df3d3a9c7a40ebf4d5b
freudnim <freud.nim@gmail.com>
2025-12-14 05:13:42 -0500

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "/freudnim-site";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: isProd ? repoName : "",
+  assetPrefix: isProd ? repoName : "",
   images: {
     unoptimized: true, // REQUIRED for GitHub Pages
   },
