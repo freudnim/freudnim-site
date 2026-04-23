3633cc5ac65f0babc3899c3968c3b2ab6b0a411a
freudnim <freud.nim@gmail.com>
2026-04-22 20:50:43 -0400

    fix link (thank you lost)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function AboutLayout() {
                     Hi! My name is Freud (pronounced 'froid' or 'frood'), but I also go by Kiri!<br /><br />
                     I'm a hobbyist illustrator who enjoys working with Vtubers on emotes, illustrations, character designs, and animations.<br /><br />
                     <Link
-                        href="https://example.com"
+                        href="http://freudnim.co/ref"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="opacity-70 hover:opacity-100 transition-all duration-200 text-blue-800"
